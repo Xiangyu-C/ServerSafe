df102cff2ab3fbef8255e58b01369f7a034bf2ab
Xiangyu Chen <xchen509@gmail.com>
2019-02-14 17:42:45 -0500

    modified ml script

--- src/spark_MLlib/spark_ml.py
+++ src/spark_MLlib/spark_ml.py
@@ -21,13 +21,10 @@ spark = SparkSession.builder.appName('Build Attack Classification Model') \
                             .master('spark://ip-10-0-0-14.ec2.internal:7077') \
                             .getOrCreate()
 sc = spark.sparkContext
-#path = 's3n://cyber-insight/cyber_attack_subset_new.csv'
 path = 's3n://cyber-insight/cyber_attack_multi.csv'
 df = spark.read.csv(path, header = True, inferSchema = True)
 
 # Get feature columns
-#feat_cols = df.columns
-#feat_cols.remove('Label')
 feat_cols =    ['Bwd Pkt Len Min',
                 'Subflow Fwd Byts',
                 'TotLen Fwd Pkts',
@@ -80,18 +77,7 @@ trained_model = rfc.fit(data_train)
 predict = trained_model.transform(data_test)
 results = converter.transform(predict)
 
-#converted = converter.transform(predict)
+converted = converter.transform(predict)
 evaluator = multi_metric(labelCol='target', predictionCol='prediction', metricName='accuracy')
 print('Accuracy is: ', evaluator.evaluate(results))
 trained_model.write().overwrite().save('s3n://cyber-insight/rfc_model_multi')
-
-# Evaluate the model using test data and output the AUC score
-# Save the trained model into s3 bucket
-#results = predict.select(['probability', 'target']).collect()
-#results2 = predict.select(['prediction', 'target']).collect()
-#results_list = [(float(i[0][0]), 1.0-float(i[1])) for i in results]
-#results_list2 = [(float(i[0][0]), 1.0-float(i[1])) for i in results]
-#score_and_labels = sc.parallelize(results_list)
-#metrics=metric(score_and_labels)
-#print('ROC score is: ', metrics.areaUnderROC)
-#trained_model.write().overwrite().save('s3n://cyber-insight/rfc_model_new')
